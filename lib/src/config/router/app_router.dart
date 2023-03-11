88938d2c569163eebb54760506542f4f4e55bc52
John Erick Santos <johnsantosdev@gmail.com>
2023-03-12 07:40:01 +1100

    setup app_router - initial page

---
+++ lib/src/config/router/app_router.dart
@@ -0,0 +1,13 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app/src/presentation/views/breaking_news_view.dart';
+
+part 'app_router.gr.dart';
+
+@AdaptiveAutoRouter(routes: [
+  // todo - for boilerplates - setup placholder view that extends HookWidget
+  AutoRoute(page: BreakingNewsView, initial: true),
+])
+class AppRouter extends _$AppRouter {}
+
+// final appRouter = AppRouter();
---
+++ lib/src/presentation/views/breaking_news_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class BreakingNewsView extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
