62d84fbce01ee94a6d27f507372092787a2c396e
John Erick Santos <johnsantosdev@gmail.com>
2023-03-14 06:27:50 +1100

    multibloc provider in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:oktoast/oktoast.dart';
 
 import './src/config/router/app_router.dart';
-import 'src/config/themes/app_themes.dart';
-import 'src/locator.dart';
-import 'src/utils/constants/strings.dart';
+import './src/config/themes/app_themes.dart';
+import './src/domain/repositories/api_repository.dart';
+import './src/locator.dart';
+import './src/presentation/cubits/remote_articles/remote_articles_cubit.dart';
+import './src/utils/constants/strings.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,13 +22,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return OKToast(
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        routerDelegate: appRouter.delegate(),
-        routeInformationParser: appRouter.defaultRouteParser(),
-        theme: AppTheme.light,
-        title: appTitle,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => RemoteArticlesCubit(locator<ApiRepository>())..getBreakingNewsArticles())
+      ],
+      child: OKToast(
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          routerDelegate: appRouter.delegate(),
+          routeInformationParser: appRouter.defaultRouteParser(),
+          theme: AppTheme.light,
+          title: appTitle,
+        ),
       ),
     );
   }
